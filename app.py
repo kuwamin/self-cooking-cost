5ee23efc1cf037d75427edca01a4ddb007491944
kuwamin <kuwamin1119@gmail.com>
2025-04-20 19:42:35 +0900

    Fix: bind to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -270,4 +270,5 @@ def get_recipe_cost(recipe_name):
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
